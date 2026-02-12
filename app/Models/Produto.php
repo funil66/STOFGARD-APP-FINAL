431a6a4f1ce159cb5a9a83be0bf5aa5d1050e563
Allisson Sousa <allisson@stofgard.com.br>
2026-02-11 21:40:02 -0300

    refactor(models): optimized Produto model imports for clean code

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 use Spatie\MediaLibrary\HasMedia;
-
+use App\Traits\HasArquivos;
 
 class Produto extends Model implements HasMedia
 {
-    use HasFactory, \App\Traits\HasArquivos;
+    use HasFactory, HasArquivos;
 
     protected $fillable = ['nome', 'descricao', 'preco_custo', 'preco_venda', 'estoque_atual', 'unidade'];
 
