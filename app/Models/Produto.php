11b31978fd43737e8186f68a528b477e71f62003
Allisson Sousa <allisson@stofgard.com.br>
2026-02-11 21:04:15 -0300

    refactor(models): use HasArquivos trait in Produto model for DRY media conversions

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -7,20 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 use Spatie\MediaLibrary\HasMedia;
-use Spatie\MediaLibrary\InteractsWithMedia;
+
 
 class Produto extends Model implements HasMedia
 {
-    use HasFactory, InteractsWithMedia;
+    use HasFactory, \App\Traits\HasArquivos;
 
     protected $fillable = ['nome', 'descricao', 'preco_custo', 'preco_venda', 'estoque_atual', 'unidade'];
 
-    public function registerMediaConversions(\Spatie\MediaLibrary\MediaCollections\Models\Media $media = null): void
-    {
-        $this->addMediaConversion('thumb')
-            ->width(200)
-            ->height(200)
-            ->sharpen(10)
-            ->nonQueued();
-    }
+
 }
