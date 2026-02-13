2bbb4b3e44c3b29d355f117ebb2d9814b6491dc0
Allisson Sousa <allisson@stofgard.com.br>
2026-02-12 23:50:44 -0300

    fix(db): disable SSL for mysqldump to fix backup errors

--- config/database.php
+++ config/database.php
@@ -61,6 +61,9 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'dump' => [
+                'add_extra_params' => '--ssl-mode=DISABLED',
+            ],
         ],
 
         'mariadb' => [
@@ -148,7 +151,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
