d9a8835fe9d49f5bfd4d85a3a01107d2da677958
Allisson Sousa <allisson@stofgard.com.br>
2026-02-13 00:11:59 -0300

    fix(backup): use local disk and clean up db config

--- config/backup.php
+++ config/backup.php
@@ -152,7 +152,7 @@ return [
              * The disk names on which the backups will be stored.
              */
             'disks' => [
-                'google',
+                'local',
             ],
         ],
 
